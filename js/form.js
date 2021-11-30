b839891b25065ca5145970d2eb17f2563839ef69
“MargaritaPavlova” <pavlovaa.margarita@gmail.com>
2021-11-30 15:37:21 +0300

    calc

---
+++ js/form.js
@@ -0,0 +1,19 @@
+const first = document.getElementById('first');
+const btn = document.getElementById('btn');
+
+console.dir(first);
+
+btn.onclick = () => {
+    alert(first.value);
+};
+
+
+
+
+
+
+// console.dir(btn);
+
+// btn.onclick = () => {
+//     alert("Alert some text");
+// };
